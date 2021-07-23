4ae839a67b1799d2c4f21b3cd0ab134e82d84e6f
Syava7 <syava77787@gmail.com>
2021-07-23 15:20:40 +0300

    first commit

---
+++ src/1-main/1-ui/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+      </BrowserRouter>
+      <>
+        {/*<Main>*/}
+      </>
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './1-main/1-ui/App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
